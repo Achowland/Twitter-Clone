76901f63219ace11c688a72969bcec317a390ba7
Achowland <achowland3911@gmail.com>
2024-07-09 16:44:57 -0400

    Add files via upload

---
+++ Twitter-Clone-Twit/Twitter-Clone-Twit/data.js
@@ -0,0 +1,58 @@
+export const tweetsData = [   
+    {
+        handle: `@KevinNotEleven32`,
+        profilePic: `images/kevin.jpg`,
+        likes: 0,
+        retweets: 1822,
+        tweetText: `Buy the latest NFTs!!, Make 💰💰💰 flipping these one of a kinds. 
+            NO SCREENSHOTS😤💢!!`,
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
+    },    
+    {
+        handle: `@devinDaPhoto ✅`,
+        profilePic: `images/reddevman01.jpg`,
+        likes: 6500,
+        retweets: 4502,
+        tweetText: `Looking for pet hamsters to model in my latest photo opp "The Smallest of Floofies", any takers?🙏`,
+        replies: [
+                  {
+                handle: `@StairMaster09`,
+                profilePic: `images/spiral.jpg`,
+                tweetText: `Would you accept an overweight mouse?🧐`,
+            },
+                  {
+                handle: `@oliveConsumer674`,
+                profilePic: `images/youngwoman01.jpg`,
+                tweetText: `Yes! message for sample shots of my lil hammy Dexter🐹❤️`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
+    },
+        {
+        handle: `@JasmineTii`,
+        profilePic: `images/flower.png`,
+        likes: 53,
+        retweets: 7,
+        tweetText: `Is there a way to remove melted candle from carpet with household items?? Asking for a friend..😓`,
+        replies: [
+            {
+                handle: `@dryHominid6776`,
+                profilePic: `images/tiedyewave.jpg`,
+                tweetText: `Have you tried melted wax?`,
+            },
+            {
+                handle: `@MomsTheBest`,
+                profilePic: `images/love.png`,
+                tweetText: `Ice bag on the area till it freezes, break it up, and vaccum it up. NEVER fails!🙌`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45',
+    },     
+]
